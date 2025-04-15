faf041ee2d514748dbc8893b93b2366f728a9307
saikarthikvarmadatla <saikarthikvarma09@gmail.com>
2025-04-15 10:58:32 -0500

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+    name        = "allow_sssh"
+    description = "Allow port number 22 for SSH access"
+
+
+    #usually we allow evrything in egress(egress is nothing but the outgoing traffic)
+     egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+     ingress {
+         from_port        = 22
+         to_port          = 22
+         protocol         = "tcp"
+         cidr_blocks      = ["0.0.0.0/0"]
+         ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_sssh"
+  }
+
+  
+}
+
+
+resource "aws_instance" "terraforminstance" {
+
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+
+    tags = {
+    Name = "terraform"
+  }
+
+  
+}
---
+++ variables/ec2.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+    name        = var.sg_name
+    description = var.sg_descr
+
+
+    #usually we allow evrything in egress(egress is nothing but the outgoing traffic)
+    #block(egress block)
+     egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+     ingress {
+         from_port        = var.from_port
+         to_port          = var.to_port
+         protocol         = var.protocol
+         cidr_blocks      = var.ingress_cidr
+         ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_sssh"
+  }
+
+  
+}
+
+
+resource "aws_instance" "terraforminstance" {
+
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+
+    tags = var.tags
+
+  
+}
